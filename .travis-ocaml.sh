c4b17d44ff411293555308b849768184277a944d
Chris Jones <clj40@cam.ac.uk>
2017-11-08 19:54:56 +0000

    Add package info to Travis script

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -1,7 +1,14 @@
-echo 'testing'
+PACKAGES = "ocamfind ounit core lwt unix"
+
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
+opam init 
+opam install ${PACKAGES}
+
+eval `opam config env`
 
 # Build the source
 sudo make
-
 # Build and run the tests
 sudo make test
