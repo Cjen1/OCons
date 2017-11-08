e0e828493a015de1d5e13e5574d66008b2e8a090
Chris Jones <clj40@cam.ac.uk>
2017-11-08 20:11:19 +0000

    Attempt to fix errors in OPAM

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -3,8 +3,9 @@ PACKAGES="ocamlfind ounit core lwt unix"
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 
-opam init 
+opam init https://opam.ocaml.org/1.1
 opam install ${PACKAGES}
+opam update
 
 eval `opam config env`
 
