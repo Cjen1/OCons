3fb60c26c5d50e313b45981aa177127ee751b67a
Chris Jones <clj40@cam.ac.uk>
2017-11-09 11:52:33 +0000

    Updat Travis script with capnproto support

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -1,15 +1,26 @@
-PACKAGES="ocamlfind ounit core lwt base-unix capnp-rpc-unix"
+PACKAGES="ocamlfind ounit core lwt base-unix"
 
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 
+# Initialize OPAM
 yes | opam init https://opam.ocaml.org/1.1
+
+# Install pre-requisite packages
 opam install ${PACKAGES}
+
+# Cap'n Proto installation (+ package)
+sudo apt-get install capnproto
+opam depext -i capnp-rpc-unix
+
+# Update all packages 
 opam update
 
+# Config environment ???
 eval `opam config env`
 
-# Build the source
+# Build the source from the Makefile
 sudo make
-# Build and run the tests
+
+# Build and run the tests from the Makefile
 sudo make test
