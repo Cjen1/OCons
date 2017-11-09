070fdadd8d16292c98f4ab46ff00387520fa27cf
Chris Jones <clj40@cam.ac.uk>
2017-11-09 11:47:23 +0000

    Update travis script with fixed package name for Unix

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -1,4 +1,4 @@
-PACKAGES="ocamlfind ounit core lwt unix"
+PACKAGES="ocamlfind ounit core lwt base-unix capnp-rpc-unix"
 
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
