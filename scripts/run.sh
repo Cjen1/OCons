ded486d396328fa424041d436a18fb2c460c6878
Chris Jones <clj40@cam.ac.uk>
2017-12-16 14:55:25 +0000

    Use jbuilder as compiler
    Switched over to a jbuilder workflow. Note:
      -     The dependency of message_api.ml/i files produces a warning at
            the moment.
      -     Currently does not build the unit test file (this will be
            remedied when the unit tests are written).

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIR=./src/_build/default/main.exe
+CONFIG=./scripts/config.json
+
+$DIR --node leader --host 127.0.0.1 --port 7000 --config $CONFIG &
+$DIR --node replica --host 127.0.0.1 --port 7010 --config $CONFIG &
+$DIR --node replica --host 127.0.0.1 --port 7011 --config $CONFIG &
+
+sleep 1
+
+$DIR --node client --host 127.0.0.1 --port 7020 --config $CONFIG &
+$DIR --node client --host 127.0.0.1 --port 7021 --config $CONFIG &
