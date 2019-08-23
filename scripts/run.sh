a7a93b9113c82c8ee79a7d04fedcca8e5a4d3ddf
Cjen1 <cjj39@cam.ac.uk>
2019-08-23 16:23:52 +0100

    Tidy repo

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,21 +4,21 @@
 killall main.exe
 
 # Buid the source
-jbuilder build --root ./src main.exe
+dune build --root ./src main.exe
 
 DIR=./src/_build/default/main.exe
 CONFIG=./scripts/config.json
 
-$DIR --node replica --host 127.0.0.1 --port 7010 --config $CONFIG &
-$DIR --node replica --host 127.0.0.1 --port 7011 --config $CONFIG &
+$DIR --node replica --host 127.0.0.1 --port 50010 --config $CONFIG &
+$DIR --node replica --host 127.0.0.1 --port 50011 --config $CONFIG &
 
-$DIR --node acceptor --host 127.0.0.1 --port 7030 --config $CONFIG &
-$DIR --node acceptor --host 127.0.0.1 --port 7031 --config $CONFIG &
-$DIR --node acceptor --host 127.0.0.1 --port 7032 --config $CONFIG &
+$DIR --node acceptor --host 127.0.0.1 --port 50030 --config $CONFIG &
+$DIR --node acceptor --host 127.0.0.1 --port 50031 --config $CONFIG &
+$DIR --node acceptor --host 127.0.0.1 --port 50032 --config $CONFIG &
 sleep 1
 
-$DIR --node leader --host 127.0.0.1 --port 7000 --config $CONFIG &
-$DIR --node leader --host 127.0.0.1 --port 7001 --config $CONFIG &
+$DIR --node leader --host 127.0.0.1 --port 50000 --config $CONFIG &
+$DIR --node leader --host 127.0.0.1 --port 50001 --config $CONFIG &
 sleep 1
 
-$DIR --node client --trace 20 --host 127.0.0.1 --port 7020 --config $CONFIG &
+$DIR --node client --trace 20 --host 127.0.0.1 --port 50020 --config $CONFIG &
