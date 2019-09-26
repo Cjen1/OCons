0e7707b71de96da1546075ffa84a257395a2b385
Cjen1 <cjj39@cam.ac.uk>
2019-09-26 19:30:00 +0100

    New server functor

---
+++ lib/server.ml
@@ -0,0 +1,57 @@
+let ( let* ) = Lwt.bind
+
+let ( and* ) = Lwt.both
+
+module type Server = sig
+  val connected_callback :
+    Lwt_io.input_channel * Lwt_io.output_channel -> unit Lwt.t
+end
+
+module Make_Server (S : Server) : sig
+  val start : Unix.inet_addr -> int -> unit Lwt.t
+end = struct
+  let string_of_sockaddr s = match s with
+    | Lwt_unix.ADDR_UNIX s -> s
+    | Lwt_unix.ADDR_INET (inet,p) -> Unix.string_of_inet_addr inet ^ ":" ^ Int.to_string p
+
+  let accept_connection (fd, sockaddr) =
+    let ic = Lwt_io.of_fd ~mode:Lwt_io.Input fd in
+    let oc = Lwt_io.of_fd ~mode:Lwt_io.Output fd in
+    let callback =
+      (* Need to ensure to close both filei descriptors after call*)
+      let* () = Logs_lwt.info (fun m -> m "Connection initialised") in
+      let* () = Logs_lwt.debug (fun m -> m "Connection initialised from %s" (string_of_sockaddr sockaddr ) ) in
+      let* () = S.connected_callback (ic, oc) in
+      (* let* () = Lwt_io.close ic and* () = Lwt_io.close oc in *)
+      let* () = Logs_lwt.info (fun m -> m "Connection finished") in
+      Lwt.return_unit
+    in
+    let () =
+      Lwt.on_failure callback (fun e ->
+          Logs.err (fun m -> m "%s" (Printexc.to_string e)))
+    in
+    let* () = Lwt.pause () in
+    Logs_lwt.info (fun m -> m "New connection")
+
+  let create_server sock =
+    let rec serve () =
+      let* conn = Lwt_unix.accept sock in
+      Lwt.join [accept_connection conn; serve ()]
+    in
+    serve
+
+  let create_socket listen_address port =
+    let open Lwt_unix in
+    let sock = socket PF_INET SOCK_STREAM 0 in
+    let* () = bind sock @@ ADDR_INET (listen_address, port) in
+    let max_pending = 20 in
+    Lwt_unix.listen sock max_pending ;
+    Lwt.return sock
+
+  let start listen_address port =
+    let serve =
+      let* sock = create_socket listen_address port in
+      create_server sock ()
+    in
+    serve
+end
