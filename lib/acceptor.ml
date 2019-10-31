fab1905e2260f30452d52a99c5b4a711cc4457d3
Chris Jensen <cjj39@cam.ac.uk>
2019-10-31 16:52:36 +0000

    Patch acceptor for new msg layer

--- lib/acceptor.ml
+++ lib/acceptor.ml
@@ -8,142 +8,49 @@ type t =
   { id: Types.unique_id
   ; mutable ballot_num: Ballot.t
   ; accepted: (Types.slot_number, Pval.t) Base.Hashtbl.t
-  ; resp_mutex: Lwt_mutex.t
-  ; wal: Lwt_io.output_channel * Unix.file_descr }
+  ; wal: Unix.file_descr
+  ; msg: Msg_layer.t }
 
-(* TODO exploit cooperative threading to remove mutexes *)
-
-let phase_1_server_callback (read, write) (acceptor : t) =
-  Logs.debug (fun m -> m "Receiving p1a") ;
-  let* msg = read () in
-  let p1a : p1a =
+let p1 t msg =
+  let p1a =
     Bytes.of_string msg |> Protobuf.Decoder.decode_exn p1a_from_protobuf
   in
-  let* () =
-    Logs_lwt.debug (fun m ->
-        m "p1: received request for ballot: %s" (Ballot.to_string p1a.ballot))
-  in
-  let* p1b =
-    critical_section acceptor.resp_mutex ~f:(fun () ->
-        let* () =
-          if Ballot.less_than acceptor.ballot_num p1a.ballot then
-            (*Ordering of wal vs update is important for durability *)
-            let* () =
-              Logs_lwt.debug (fun m ->
-                  m "p1: updating ballot to %s" (Ballot.to_string p1a.ballot))
-            in
-            let* () =
-              acceptor.ballot_num |> Ballot.to_string
-              |> write_to_wal acceptor.wal
-            in
-            Lwt.return @@ (acceptor.ballot_num <- p1a.ballot)
-          else Lwt.return ()
-        in
-        let p1b : p1b =
-          { ballot= acceptor.ballot_num
-          ; accepted=
-              Base.Hashtbl.data acceptor.accepted
-              (* TODO reduce this? Can be done by including a high water mark in p1a message decisions *)
-          }
-        in
-        Lwt.return p1b)
-  in
-  Logs.debug (fun m -> m "Sending p1b") ;
-  let p1b_string =
-    p1b |> Protobuf.Encoder.encode_exn p1b_to_protobuf |> Bytes.to_string
-  in
-  let* () = write p1b_string in
-  Logs_lwt.debug (fun m -> m "Sent p1b")
+  if Ballot.less_than t.ballot_num p1a.ballot then (
+    p1a.ballot |> Ballot.to_string |> write_to_wal t.wal ;
+    t.ballot_num <- p1a.ballot ) ;
+  { ballot= t.ballot_num
+  ; accepted=
+      Base.Hashtbl.data t.accepted
+      (* TODO reduce this? Can be done by including a high water mark in p1a message decisions *)
+  }
+  |> Protobuf.Encoder.encode_exn p1b_to_protobuf
+  |> Bytes.to_string
+  |> Msg_layer.send_msg t.msg ~filter:"p1b"
 
-exception Preempted of Ballot.t
-
-let phase_2_server_callback (read, write) (acceptor : t) =
-  let* msg = read () in
-  let p2a : p2a =
-    msg |> Bytes.of_string |> Protobuf.Decoder.decode_exn p2a_from_protobuf
+let p2 t msg =
+  let p2a =
+    Bytes.of_string msg |> Protobuf.Decoder.decode_exn p2a_from_protobuf
   in
   let ((ib, is, _) as ipval) = p2a.pval in
-  let* () =
-    Logs_lwt.debug (fun m ->
-        m "p2_callback: got pval from leader: %s" (Pval.to_string ipval))
-  in
-  try%lwt
-    let* () =
-      critical_section acceptor.resp_mutex ~f:(fun () ->
-          if not (Ballot.less_than ib acceptor.ballot_num) then
-            let should_update =
-              (*match Base.Hashtbl.find acceptor.accepted is with
-                | Some (b', _, _) ->
-                    if
-                      not (Ballot.less_than ib b') (* ~(ib < b') = ib >= b' *)
-                      (* If more up to date ballot number was received *)
-                    then true
-                    else (
-                      Logs.debug (fun m ->
-                          m
-                            "phase_2_server_callback: assert incomming ballot = current \
-                             ballot: (%s = %s)"
-                            (Ballot.to_string ib)
-                            (Ballot.to_string acceptor.ballot_num)) ;
-                      assert false )
-                (* if else branch then there exists a ballot with a ballot > ballot_num which has been accepted already *)
-                | None ->
-                    true
-                  *)
-              true
-            in
-            if should_update then
-              let* () = Logs_lwt.debug (fun m -> m "p2: writing to wal") in
-              let* () = write_to_wal acceptor.wal @@ Pval.to_string ipval in
-              let* () =
-                Logs_lwt.debug (fun m ->
-                    m "p2: Committed %s" (Pval.to_string ipval))
-              in
-              Lwt.return
-              @@ Base.Hashtbl.set acceptor.accepted ~key:is ~data:ipval
-            else Lwt.return_unit
-          else raise (Preempted acceptor.ballot_num))
-    in
-    let p2b : p2b = {acceptor_id= acceptor.id; ballot= acceptor.ballot_num} in
-    let marshalled_resp =
-      p2b |> Protobuf.Encoder.encode_exn p2b_to_protobuf |> Bytes.to_string
-    in
-    write marshalled_resp
-  with Preempted b ->
-    let* () =
-      Logs_lwt.debug (fun m ->
-          m "p2: preempted by ballot: %s" (Ballot.to_string b))
-    in
-    let nack : nack = {ballot= b} in
-    let nack_string =
-      nack |> Protobuf.Encoder.encode_exn nack_to_protobuf |> Bytes.to_string
-    in
-    write nack_string
+  let ( >= ) a b = not @@ Ballot.less_than a b in
+  if ib >= t.ballot_num then (
+    write_to_wal t.wal @@ Pval.to_string ipval ;
+    Base.Hashtbl.set t.accepted ~key:is ~data:ipval ;
+    {acceptor_id= t.id; ballot= t.ballot_num}
+    |> Protobuf.Encoder.encode_exn p2b_to_protobuf
+    |> Bytes.to_string
+    |> Msg_layer.send_msg t.msg ~filter:"p2b" )
 
-(* Initialize a new acceptor *)
-let create wal_location =
+let create wal_loc msg = 
   { id= Types.create_id ()
   ; ballot_num= Ballot.bottom ()
   ; accepted= Base.Hashtbl.create (module Base.Int)
-  ; resp_mutex= Lwt_mutex.create ()
   ; wal=
-      (let fd =
-         Unix.openfile wal_location [Unix.O_RDWR; Unix.O_CREAT]
+         Unix.openfile wal_loc [Unix.O_RDWR; Unix.O_CREAT]
          @@ int_of_string "0x660"
-       in
-       let chan =
-         Lwt_io.of_fd ~mode:Lwt_io.Output (Lwt_unix.of_unix_file_descr fd)
-       in
-       (chan, fd)) }
+  ; msg = msg}
 
-let start acceptor host p1_port p2_port =
-  Lwt.join
-    [ Server.start host p1_port acceptor phase_1_server_callback
-    ; Server.start host p2_port acceptor phase_2_server_callback ]
-
-(* Creating a new acceptor consists of initializing a record for it and
-   starting a server for accepting incoming messages *)
-let create_and_start_acceptor host p1_port p2_port wal_location =
-  let* () = Logs_lwt.info (fun m -> m "Spinning up acceptor") in
-  let acceptor = create wal_location in
-  start acceptor host p1_port p2_port
+(* Initialize a new acceptor *)
+let create_independent wal_loc nodes local =
+  let msg=Msg_layer.create nodes local in
+  create wal_loc msg
