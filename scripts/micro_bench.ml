f5e01e97d3bcd26db7f94d49f8c5bd52ea147891
Chris Jensen <cjj39@cam.ac.uk>
2021-03-12 13:26:29 +0000

    Update script

--- scripts/micro_bench.ml
+++ scripts/micro_bench.ml
@@ -14,6 +14,10 @@ let set_core_profiler output = set_env "CORE_PROFILER" ("OUTPUT_FILE=" ^ output)
 
 let if_exists f file = if%bind file_exists file then f file else return ()
 
+let delay t p =
+  let%bind () = sleep t in
+  p
+
 let start_server node_id =
   let data_dir = Fmt.str "%d.datadir" node_id in
   let%bind () = if_exists rm_rf data_dir in
@@ -57,9 +61,7 @@ let print_summary file =
   call ["core-profiler-tool"; "summary"; "-filename"; file]
 
 let test target_rate =
-  let%bind () = print @@ Fmt.str "Built main\n" in
-  let%bind () = sleep 1. in
-  let%bind bgs = fork_all [start_server 1; start_server 2] in
+  let%bind bgs = fork_all [start_server 1; delay 1. @@ start_server 2] in
   let%bind exit_code = client_test [1; 2] target_rate in
   let%bind () = List'.iter bgs ~f:kill in
   match exit_code with
@@ -77,7 +79,12 @@ let () =
         flag "rate" (optional_with_default 5000 int) ~doc:"N Target rate"
       and trace = flag "trace" no_arg ~doc:" Print a trace of the test" in
       fun () ->
-        let () = eval (call ["dune"; "build"; "bin/main.exe"] ) in
+        let () =
+          eval
+            (call ["dune"; "build"; "bin/main.exe"; "scripts/micro_bench.exe"])
+        in
+        let () = eval (sleep 5.) in
+        let () = eval (print @@ Fmt.str "Built main\n") in
         match trace with
         | true ->
             let (), trace = Traced.eval_exn @@ test rate in
