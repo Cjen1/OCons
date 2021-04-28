ff7868ce02ae3c78b94574484345e8b88c8b9b2e
Chris Jensen <cjj39@cam.ac.uk>
2021-04-28 17:22:52 +0100

    Update client

--- bin/client.ml
+++ bin/client.ml
@@ -1,51 +1,71 @@
-(*open! Core
+open! Core
 open! Async
-open Ocamlpaxos
-module C = Client
+module O = Ocons_core
 
 let bytes = Command.Arg_type.create @@ Bytes.of_string
 
-let print_res res = res |> [%sexp_of: Types.op_result] |> Sexp.to_string_hum
+let network_address =
+  let parse s =
+    match String.split ~on:':' s with
+    | [addr; port] ->
+        (Fmt.str "%s:%s" addr port)
+    | _ ->
+        Fmt.failwith "Expected ip:port | path rather than %s " s
+  in
+  Command.Arg_type.create parse
+
+let node_list =
+  Command.Arg_type.comma_separated ~allow_empty:false network_address
+
+let print_res res = res |> [%sexp_of: O.Types.op_result] |> Sexp.to_string_hum
+
+let log_param =
+  Log_extended.Command.(
+    setup_via_params ~log_to_console_by_default:(Stderr Color)
+      ~log_to_syslog_by_default:false ())
 
 let put =
   Command.async_spec ~summary:"Put request"
     Command.Spec.(
       empty
-      +> flag "-p" ~doc:"ports list" (listed string)
+      +> anon ("node list" %: node_list)
       +> anon ("key" %: bytes)
-      +> anon ("value" %: bytes))
-    (fun ps k v () ->
-      let c = C.new_client ps in
-      let%map res = C.op_write c ~k ~v in
-      res |> [%sexp_of: Types.op_result] |> Sexp.to_string_hum |> print_endline
+      +> anon ("value" %: bytes)
+      +> log_param)
+    (fun ps k v () () ->
+      let global_level = Async.Log.Global.level () in
+      let global_output = Async.Log.Global.get_output () in
+      List.iter
+        [ O.Utils.logger
+        ; O.Client.logger
+        ] ~f:(fun log ->
+          Async.Log.set_level log global_level ;
+          Async.Log.set_output log global_output ) ;
+      let c = O.Client.new_client ps in
+      let%map res = O.Client.op_write c ~k ~v in
+      res |> print_res |> print_endline
       )
 
 let get =
   Command.async_spec ~summary:"Get request"
     Command.Spec.(
       empty
-      +> flag "-p" ~doc:"ports list" (listed string)
-      +> anon ("key" %: bytes))
-    (fun ps k () ->
-      let c = C.new_client ps in
-      let%map res = C.op_read c k in
-      res |> [%sexp_of: Types.op_result] |> Sexp.to_string_hum |> print_endline
+      +> anon ("node list" %: node_list)
+      +> anon ("key" %: bytes)
+      +> log_param)
+    (fun ps k () () ->
+      let global_level = Async.Log.Global.level () in
+      let global_output = Async.Log.Global.get_output () in
+      List.iter
+        [ O.Utils.logger
+        ; O.Client.logger
+        ] ~f:(fun log ->
+          Async.Log.set_level log global_level ;
+          Async.Log.set_output log global_output ) ;
+      let c = O.Client.new_client ps in
+      let%map res = O.Client.op_read c k in
+      res |> print_res |> print_endline
       )
-
-let reporter =
-  let report src level ~over k msgf =
-    let k _ = over () ; k () in
-    let src = Logs.Src.name src in
-    msgf
-    @@ fun ?header ?tags:_ fmt ->
-    Fmt.kpf k Fmt.stdout
-      ("[%a] %a %a @[" ^^ fmt ^^ "@]@.")
-      Time.pp (Time.now ())
-      Fmt.(styled `Magenta string)
-      (Printf.sprintf "%14s" src)
-      Logs_fmt.pp_header (level, header)
-  in
-  {Logs.report}
 
 (* Handle the command line arguments and run application is specified mode *)
 let cmd =
@@ -54,6 +74,5 @@ let cmd =
 
 let () =
   Fmt_tty.setup_std_outputs () ;
-  Logs.(set_level (Some Debug)) ;
-  Logs.set_reporter reporter ; Core.Command.run cmd
-  *)
+  Fmt.pr "%a" (Fmt.array ~sep:Fmt.sp Fmt.string) (Sys.get_argv ()) ;
+  Rpc_parallel.start_app cmd
