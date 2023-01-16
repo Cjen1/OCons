50f2eae196c1b8ccece2c7846ebc8513a6be9816
cjen1 <cjj39@cam.ac.uk>
2023-01-16 15:30:56 +0000

    Entries ser/parse

--- paxos/fuzz/test_msgs.ml
+++ paxos/fuzz/test_msgs.ml
@@ -21,9 +21,69 @@ module Gen = struct
     map [command; int] (fun command term -> Ocons_core.Types.{command; term})
 end
 
-module LP = struct
-  open Paxos_core.Line_prot
-end
+module LP = Paxos_core.Line_prot
+
+let make_source q =
+  object (self)
+    inherit Eio.Flow.source
+
+    val q = q
+
+    val mutable left_over : Cstruct.t option = None
+
+    method read_into buf =
+      let copy_and_assign_rem data buf =
+        match (Cstruct.length data, Cstruct.length buf) with
+        | ld, lb when ld <= lb ->
+            Cstruct.blit data 0 buf 0 ld ;
+            ld
+        | ld, lb ->
+            Cstruct.blit data 0 buf 0 lb ;
+            let rem = Cstruct.take ~min:(ld - lb) data in
+            left_over <- Some rem ;
+            lb
+      in
+      match left_over with
+      | Some data ->
+          copy_and_assign_rem data buf
+      | None ->
+          copy_and_assign_rem (Eio.Stream.take q) buf
+  end
+
+let make_sink q =
+  object (self)
+    inherit Eio.Flow.sink
+
+    method copy src =
+      try
+        while true do
+          let buf = Cstruct.create 4096 in
+          let got = src#read_into buf in
+          Eio.Stream.add q (Cstruct.split buf got |> fst)
+        done
+      with End_of_file -> ()
+
+    method! write bufs = List.iter (fun buf -> Eio.Stream.add q buf) bufs
+  end
+
+let mock_flow () =
+  let q = Eio.Stream.create 8 in
+  (make_source q, make_sink q)
+
+let test_entry_equality les =
+  let open Crowbar in
+  Eio_mock.Backend.run
+  @@ fun () ->
+  let length = List.length les in
+  let w_entries = (Iter.of_list les, length) in
+  (*guard (LP.Size.entries w_entries < 1024) ;*)
+  let fr, fw = mock_flow () in
+  let br = Eio.Buf_read.of_flow ~max_size:65536 fr in
+  Eio.Buf_write.with_flow fw
+  @@ fun bw ->
+  LP.SerPrim.entries w_entries bw ;
+  let r_entries = LP.DeserPrim.entries br in
+  check_eq ~eq:entries_equal w_entries r_entries
 
 let () =
   let open Crowbar in
@@ -31,8 +91,5 @@ let () =
     [list Gen.log_entry]
     (fun l ->
       let e = (Iter.of_list l, List.length l) in
-      check_eq ~eq:entries_equal e e );
-  add_test ~name:"entries_ser_deser"
-    [list Gen.log_entry]
-    (fun les ->
-      let length 
+      check_eq ~eq:entries_equal e e ) ;
+  add_test ~name:"entries_ser_deser" [list Gen.log_entry] test_entry_equality
--- paxos/lib/line_prot.ml
+++ paxos/lib/line_prot.ml
@@ -3,18 +3,20 @@ open! Utils
 module W = Eio.Buf_write
 module R = Eio.Buf_read
 
+module CommonPrim = struct
+  let sm_op_to_uint8 =
+    let open C.Types in
+    function Read _ -> 0 | Write _ -> 1 | CAS _ -> 2 | NoOp -> 3
+end
+
 module SerPrim = struct
   let string s w =
     W.BE.uint64 w (s |> String.length |> Int64.of_int) ;
     W.string w s
 
-  let enum_sm_op =
-    let open C.Types in
-    function Read _ -> 0 | Write _ -> 1 | CAS _ -> 2 | NoOp -> 3
-
   let sm_op op w =
     let open C.Types in
-    W.uint8 w (enum_sm_op op) ;
+    W.uint8 w (CommonPrim.sm_op_to_uint8 op) ;
     match op with
     | Read k ->
         string k w
@@ -29,7 +31,7 @@ module SerPrim = struct
     W.BE.uint64 w (Int64.of_int id) ;
     sm_op op w
 
-  let serialise_entries (es, length) w =
+  let entries (es, length) w =
     W.BE.uint64 w (Int64.of_int length) ;
     es
     |> Iter.iter (fun C.Types.{term; command= cmd} ->
@@ -37,8 +39,73 @@ module SerPrim = struct
            command cmd w )
 end
 
+module Size = struct
+  let string s = 8 + String.length s
+
+  let sm_op op =
+    let open C.Types in
+    1
+    +
+    match op with
+    | Read k ->
+        string k
+    | Write (k, v) ->
+        string k + string v
+    | CAS {key; value; value'} ->
+        string key + string value + string value'
+    | NoOp ->
+        0
+
+  let command C.Types.Command.{op; _} = 8 + sm_op op
+
+  let entries (es, _) =
+    8 + Iter.fold (fun s C.Types.{command= cmd; _} -> s + 8 + command cmd) 0 es
+end
+
 module DeserPrim = struct
   open! R.Syntax
+
+  type 'a parser = 'a R.parser
+
+  let string : string parser =
+    let* len = R.BE.uint64 in
+    R.take (Int64.to_int len)
+
+  let sm_op : C.Types.sm_op parser =
+    let* op_code = R.map Char.code R.any_char in
+    match op_code with
+    | 0 (* Read *) ->
+        let* k = string in
+        R.return @@ C.Types.Read k
+    | 1 (* Write *) ->
+        let* k = string and* v = string in
+        R.return @@ C.Types.Write (k, v)
+    | 2 (* CAS *) ->
+        let* key = string and* value = string and* value' = string in
+        R.return @@ C.Types.(CAS {key; value; value'})
+    | 3 (* NoOp *) ->
+        R.return @@ C.Types.NoOp
+    | _ ->
+        raise
+          (Invalid_argument
+             (Fmt.str "Received %d which is not a valid op_code" op_code) )
+
+  let command =
+    let* id = R.map Int64.to_int R.BE.uint64 in
+    let* op = sm_op in
+    R.return C.Types.Command.{op; id}
+
+  let entries r =
+    let len = R.BE.uint64 r |> Int64.to_int in
+    let arr =
+      Array.init len (fun _ -> C.Types.{term= -1; command= empty_command})
+    in
+    for i = 0 to len - 1 do
+      let term = R.BE.uint64 r |> Int64.to_int in
+      let command = command r in
+      Array.set arr i C.Types.{term; command}
+    done ;
+    (Iter.of_array arr, len)
 end
 
 let enum_msg = function
@@ -62,6 +129,6 @@ let serialise m w =
       W.BE.uint64 w (of_int term) ;
       W.BE.uint64 w (of_int start_index) ;
       W.BE.uint64 w (of_int length) ;
-      SerPrim.serialise_entries (entries, length) w
+      SerPrim.entries (entries, length) w
   | AppendEntries _ | AppendEntriesResponse _ ->
       assert false
