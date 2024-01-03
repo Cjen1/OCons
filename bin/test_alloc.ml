4017abcd3d917046f08a579a95d924c169f4a6ed
cjen1 <cjj39@cam.ac.uk>
2024-01-03 10:57:44 +0000

    Test allocation options

--- bin/test_alloc.ml
+++ bin/test_alloc.ml
@@ -13,4 +13,30 @@ let alloc_test =
   in
   Bench.Test.create_with_initialization ~name:"alloc_with_reference" test
 
-let () = Command_unix.run (Bench.make_command [alloc_test])
+let alloc_return_tuple =
+  let extern = ref 0 in
+  let f x = (x + 1, x + 2, x + 3) in
+  let test () =
+    let a, b, c = f 1 in
+    extern := a + b + c
+  in
+  Bench.Test.create ~name:"alloc_return_tuple" test
+
+type foo = {mutable a: int; mutable b: int; mutable c: int}
+
+let alloc_return_struct =
+  let extern = ref 0 in
+  let f x r =
+    r.a <- x + 1 ;
+    r.b <- x + 2 ;
+    r.c <- x + 3
+  in
+  let test `init =
+    let foo = {a= 0; b= 0; c= 0} in
+    fun () ->
+      f 1 foo ;
+      extern := foo.a + foo.b + foo.c
+  in
+  Bench.Test.create_with_initialization ~name:"alloc_return_struct" test
+
+let () = Command_unix.run (Bench.make_command [alloc_test; alloc_return_tuple; alloc_return_struct])
